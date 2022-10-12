b077b94553bc90e29322f6ba795ccb8b8648317d
savioferraz <savio.ferraz88@gmail.com>
2022-10-12 12:36:02 -0300

    prepare for deploy

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,17 @@
-import pkg from "pg";
+import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const { Pool } = pkg;
+const { Pool } = pg;
 
-const connection = new Pool({ onnectionString: process.env.DATABASE_URL });
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
-export { connection };
+const connection = new Pool(databaseConfig);
+
+export default connection;
